7bfb08bf3ddb1a4998259ecf0555e61bd904679d
Vladislav Reznik <vlreznik97@gmail.com>
2023-01-27 15:56:15 +0300

    Release v.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='step_driver_g071_api',
-    version='0.1',
+    version='0.5',
     packages=['step_driver_g071_api'],
     url='https://github.com/gelio5/step_driver_G071_API.git',
     license='MIT',
